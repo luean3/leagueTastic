c6c73fed212762451a864016ddb69ee1672b9788
nicola <nicola.hofstetter@bluewin.ch>
2026-05-09 10:33:59 +0200

    fixed function

--- functions/src/handlers/stravaWebhook.ts
+++ functions/src/handlers/stravaWebhook.ts
@@ -1,31 +1,35 @@
 import {onRequest} from "firebase-functions/https";
 import * as admin from "firebase-admin";
 import {logger} from "firebase-functions";
+import {STRAVA_CLIENT_ID, STRAVA_CLIENT_SECRET} from "../config/strava";
 
-export const stravaWebhook = onRequest(async (req, res) => {
-    if (req.method === "GET") {
-        res.json({ "hub.challenge": req.query["hub.challenge"] });
-        return;
-    }
+export const stravaWebhook = onRequest(
+    {
+        secrets: [STRAVA_CLIENT_ID, STRAVA_CLIENT_SECRET],
+    }, async (req, res) => {
+        if (req.method === "GET") {
+            res.json({"hub.challenge": req.query["hub.challenge"]});
+            return;
+        }
 
-    const body = req.body;
+        const body = req.body;
 
-    if (body.object_type !== "activity") {
-        res.status(200).send("ignored");
-        return;
-    }
+        if (body.object_type !== "activity") {
+            res.status(200).send("ignored");
+            return;
+        }
 
-    // 👉 nur enqueue
-    await admin.firestore().collection("jobs").add({
-        type: "processActivity",
-        activityId: body.object_id,
-        athleteId: body.owner_id,
-        createdAt: Date.now()
+        // 👉 nur enqueue
+        await admin.firestore().collection("jobs").add({
+            type: "processActivity",
+            activityId: body.object_id,
+            athleteId: body.owner_id,
+            createdAt: Date.now()
+        });
+        logger.log({
+            message: "saved job",
+            activityId: body.object_id,
+            athleteId: body.owner_id,
+        });
+        res.status(200).send("ok");
     });
-    logger.log({
-        message: "saved job",
-        activityId: body.object_id,
-        athleteId: body.owner_id,
-    });
-    res.status(200).send("ok");
-});
